d145828932848ec445d63dfcaf80acf32afd2426
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-04 18:59:02 +0100

    Adds empty implementations of Module functions so tests can run

---
+++ src/main/pasta_module.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+#include "pasta_module.h"
+
+Module *pasta_module_create()
+{
+    return NULL;
+}
+
+Status pasta_module_destroy(Module *const module_p)
+{
+    return PASTA_ERROR_NOT_IMPLEMENTED;
+}
+
+Status pasta_module_set_name(Module *const module_p, const char *name)
+{
+    return PASTA_ERROR_NOT_IMPLEMENTED;
+}
+
+Status pasta_module_set_command(Module *const module_p, const char *command)
+{
+    return PASTA_ERROR_NOT_IMPLEMENTED;
+}
+
+Status pasta_module_set_interval(Module *const module_p, int interval)
+{
+    return PASTA_ERROR_NOT_IMPLEMENTED;
+}
+
+Status pasta_module_set_state(Module *const module_p, ModuleState state)
+{
+    return PASTA_ERROR_NOT_IMPLEMENTED;
+}

8cf2c0736dcf26699f4b1d08084abfe825e6c66f
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-11 23:59:21 +0100

    Updates pasta config comments

--- src/include/pasta_config_parser.h
+++ src/include/pasta_config_parser.h
@@ -19,8 +19,16 @@ void pasta_config_reset_allocator();
  * pasta_load_modules
  * 
  * Opens, reads and parses the specified configuration file and returns a 
- * dynamically allocated array of the Modules that was loaded. 
+ * dynamically allocated array of the Modules that was loaded.
+ *
+ * returns  PASTA_ERROR_INVALID_ARGUMENT if 'modules' is NOT NULL,
+ *          PASTA_ERROR_CONFIG_FORMAT if the loaded config file has incorrect formatting,
+ *          PASTA_ERROR_FILE_NOT_FOUND if the file at 'filepath' could not be found,
+ *          PASTA_ERROR_PERMISSION_DENIED if the program did not have permission to open the file at 'filepath',
+ *          PASTA_ERROR_ALLOCATION_FAILED if allocation of resources failed,
+ *          PASTA_WARNING_NO_MODULES if there were no modules specified in the configuration file,
+ *          PASTA_SUCCESS otherwise
  */
-Status pasta_config_load_modules(Module *mod[], int *loaded_modules, const char *filepath);
+Status pasta_config_load_modules(Module *modules[], int *loaded_modules, const char *filepath);
 
 #endif /* PASTA_CONFIG_PARSER_H */

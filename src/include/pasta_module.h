20c286c27819e73d599c369a2cb633ec76e2a758
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-11 23:50:58 +0100

    Updates Module comments

--- src/include/pasta_module.h
+++ src/include/pasta_module.h
@@ -10,7 +10,7 @@
 
 #include <stddef.h> // size_t
 
-#include "pasta_status_codes.h"
+#include "pasta_status_codes.h" // Status
 
 #define PASTA_MODULE_MAX_NAME_LEN 100
 #define PASTA_MODULE_MAX_CMD_LEN 1000
@@ -34,10 +34,54 @@ struct Module
 
 typedef struct Module Module;
 
+/*
+ * Initializes a module to default values. 
+ *
+ * Default values are: 
+ * name: "", command: "", interval_seconds: 0, state: Stopped
+ *
+ * returns  PASTA_ERROR_NULL_ARGUMENT if 'module_p' is NULL,
+ *          PASTA_SUCCESS otherwise
+ */
 Status pasta_module_init(Module *const module_p);
+
+/*
+ * Sets the name of a module.
+ *
+ * returns  PASTA_ERROR_NULL_ARGUMENT if 'module_p' or 'name' is NULL,
+ *          PASTA_ERROR_INVALID_ARGUMENT if 'name' is empty, 'name_len' is 0 or 'name' contains non printable ASCII symbols
+ *          PASTA_ERROR_BUFFER_OVERFLOW if 'name_len' is > PASTA_MODULE_MAX_NAME_LEN,
+ *          PASTA_SUCCESS otherwise
+ */
 Status pasta_module_set_name(Module *const module_p, const char *name, size_t name_len);
+
+/*
+ * Sets the command of a module.
+ *
+ * returns  PASTA_ERROR_NULL_ARGUMENT if 'module_p' or 'name' is NULL,
+ *          PASTA_ERROR_INVALID_ARGUMENT if 'command' is empty or 'cmd_len' is 0,
+ *          PASTA_ERROR_BUFFER_OVERFLOW if 'cmd_len' is > PASTA_MODULE_MAX_CMD_LEN,
+ *          PASTA_SUCCESS otherwise
+ */
 Status pasta_module_set_command(Module *const module_p, const char *command, size_t cmd_len);
+
+/*
+ * Sets the interval, in seconds, of a module. The interval specifies how often the command associated with a module
+ * should be executed.
+ *
+ * returns  PASTA_ERROR_NULL_ARGUMENT if 'module_p' is NULL,
+ *          PASTA_ERROR_INVALID_ARGUMENT if interval is < 0
+ *          PASTA_SUCCESS, otherwise
+ */
 Status pasta_module_set_interval(Module *const module_p, int interval);
+
+/*
+ * Sets the state of a module.
+ *
+ * returns  PASTA_ERROR_NULL_ARGUMENT if 'module_p' is NULL,
+ *          PASTA_ERROR_INVALID_ARGUMENT if state is < 0 or >= PASTA_MODULE_STATE_VALUES
+ *          PASTA_SUCCESS, otherwise
+ */
 Status pasta_module_set_state(Module *const module_p, ModuleState state);
 
 #endif /* PASTA_MODULE_H */

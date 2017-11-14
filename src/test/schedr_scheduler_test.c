5069b87791e1eb9b0de0cd645835b56ce65f7f97
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-14 20:36:53 +0100

    Adds some empty test functions to scheduler test

--- src/test/schedr_scheduler_test.c
+++ src/test/schedr_scheduler_test.c
@@ -10,11 +10,27 @@
 
 static bool process_exists(pid_t pid);
 
-static void start_job_should_start_a_new_process_when_job_has_valid_command();
+static void start_job_should_start_a_new_process();
+static void start_job_should_run_executable_file();
+static void start_job_should_run_job_every_sec();
+static void start_job_should_execute_correct_command_when_command_has_many_arguments();
+static void start_job_should_execute_correct_command_when_command_contains_shell_builtins();
+static void start_job_should_return_permission_denied_error_when_not_permitted_to_run_command();
+static void start_job_should_return_command_not_found_error_when_command_does_not_exist();
+
+static void stop_job_should_terminate_process();
 
 int main(void)
 {
-    start_job_should_start_a_new_process_when_job_has_valid_command();
+    start_job_should_start_a_new_process();
+    start_job_should_run_executable_file();
+    start_job_should_run_job_every_sec();
+    start_job_should_execute_correct_command_when_command_has_many_arguments();
+    start_job_should_execute_correct_command_when_command_contains_shell_builtins();
+    start_job_should_return_permission_denied_error_when_not_permitted_to_run_command();
+    start_job_should_return_command_not_found_error_when_command_does_not_exist();
+
+    stop_job_should_terminate_process();
 
     test_print_summary();
 
@@ -35,7 +51,7 @@ static bool process_exists(pid_t pid)
     }
 }
 
-static void start_job_should_start_a_new_process_when_job_has_valid_command()
+static void start_job_should_start_a_new_process()
 {
     Job job = {.name = "testing", .command = "echo hello", .interval_seconds = 1, .state = Stopped};
 
@@ -44,3 +60,44 @@ static void start_job_should_start_a_new_process_when_job_has_valid_command()
 
     test_assert(process_exists(job_pid) && status == SCHEDR_SUCCESS);
 }
+
+static void start_job_should_run_executable_file()
+{
+    test_fail();
+}
+
+static void start_job_should_run_job_every_sec()
+{
+    test_fail();
+}
+
+static void start_job_should_run_executable_file()
+{
+    test_fail();
+}
+
+static void start_job_should_execute_correct_command_when_command_has_many_arguments()
+{
+    test_fail();
+}
+
+static void start_job_should_execute_correct_command_when_command_contains_shell_builtins()
+{
+    test_fail();
+}
+
+static void start_job_should_return_permission_denied_error_when_not_permitted_to_run_command()
+{
+    test_fail();
+}
+
+static void start_job_should_return_command_not_found_error_when_command_does_not_exist()
+{
+    test_fail();
+}
+
+static void stop_job_should_terminate_process()
+{
+    test_fail();
+}
+

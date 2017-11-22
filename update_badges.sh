0ee312cb4ed27c15aae5cf6553d5db9bc0d1e688
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-22 16:07:52 +0100

    Fix bug with update badges script that made badge have wrong colors

--- update_badges.sh
+++ update_badges.sh
@@ -19,32 +19,32 @@ do
     fi
 done <<< $(make -s test)
 
-if (( $tests_failed == 0 ))
+if (( $tests_failed > 0 ))
 then
     test_badge_color="red"
 fi
 
 if (( $coverage < 50 ))
 then
-    color="red"
+    cov_badge_color="red"
 elif (( $coverage < 70 ))
 then
-    color="orange"
+    cov_badge_color="orange"
 elif (( $coverage < 80 ))
 then
-    color="yellow"
+    cov_badge_color="yellow"
 elif (( $coverage < 90 ))
 then
-    color="yellowgreen"
+    cov_badge_color="yellowgreen"
 elif (( $coverage < 95 ))
 then
-    color="green"
+    cov_badge_color="green"
 else
-    color="brightgreen"
+    cov_badge_color="brightgreen"
 fi
 
-cov_url=https://img.shields.io/badge/${cov_badge_name}-${coverage}%25-${color}.svg\?style=for-the-badge
-test_url=https://img.shields.io/badge/${test_badge_name}-${tests_passed}%20passed%20${tests_failed}%20failed-${color}.svg\?style=for-the-badge
+cov_url=https://img.shields.io/badge/${cov_badge_name}-${coverage}%25-${cov_badge_color}.svg\?style=for-the-badge
+test_url=https://img.shields.io/badge/${test_badge_name}-${tests_passed}%20passed%20${tests_failed}%20failed-${test_badge_color}.svg\?style=for-the-badge
 
 wget -q -O doc/coverage.svg "$cov_url"
 wget -q -O doc/tests.svg "$test_url"

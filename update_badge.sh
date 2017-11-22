dc653850181dc639a60792fcc6bcd4d598c6bdb5
Daniel Alm Grundström <daniel.alm.grundstrom@live.se>
2017-11-22 13:42:35 +0100

    Adds tests passed/failed badge

--- update_badge.sh
+++ update_badge.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-badge_name="coverage"
-
+cov_badge_name="coverage"
+cov_badge_color="lightgrey"
+test_badge_name="tests"
+test_badge_color="brightgreen"
 coverage=$(make -s cov | grep TOTAL | awk {'print $4'} | cut -d '%' -f 1)
-color="lightgrey"
+tests_passed=0
+tests_failed=0
+
+while read line
+do
+    if [[ ! -z $line ]]
+    then
+        passed=$(echo "$line" | grep -o "[0-9]\+ succeeded" | cut -d ' ' -f 1)
+        failed=$(echo "$line" | grep -o "[0-9]\+ failed" | cut -d ' ' -f 1)
+        let tests_passed+=${passed}
+        let tests_failed+=${failed}
+    fi
+done <<< $(make -s test)
+
+if (( $tests_failed == 0 ))
+then
+    test_badge_color="red"
+fi
 
 if (( $coverage < 50 ))
 then
@@ -24,6 +43,9 @@ else
     color="brightgreen"
 fi
 
-url=https://img.shields.io/badge/${badge_name}-${coverage}%25-${color}.svg\?style=for-the-badge
+cov_url=https://img.shields.io/badge/${cov_badge_name}-${coverage}%25-${color}.svg\?style=for-the-badge
+test_url=https://img.shields.io/badge/${test_badge_name}-${tests_passed}%20passed%20${tests_failed}%20failed-${color}.svg\?style=for-the-badge
 
-wget -q -O doc/coverage.svg $url
+wget -q -O doc/coverage.svg "$cov_url"
+wget -q -O doc/tests.svg "$test_url"
+

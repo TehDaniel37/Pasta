1f635b1775cdcdfee8c419acade007b128f77f5d
Daniel Alm Grundstrom <daniel.alm.grundstrom@protonmail.com>
2018-01-27 17:31:18 +0100

    Temp update

--- token_prototype/main.c
+++ token_prototype/main.c
@@ -6,14 +6,6 @@
 
 #define MAX_TOKENS 20
 
-struct Token {
-    char name[100];
-    regex_t *pattern;
-    void (*handler)(Job *, char *);
-};
-
-typedef struct Token Token;
-
 struct Job {
     char name[100];
     char cmd[100];
@@ -22,6 +14,14 @@ struct Job {
 
 typedef struct Job Job;
 
+struct Token {
+    char name[100];
+    regex_t *pattern;
+    void (*handler)(Job *, char *);
+};
+
+typedef struct Token Token;
+
 static Token tokens[MAX_TOKENS];
 static int nr_of_tokens;
 
@@ -29,34 +29,45 @@ static Token create_token(const char *name, const char *pattern, void (*handler)
 
 static void job_handler(Job *current_job, char *token)
 {
-    strcpy(current_job->name, "");
+    printf("Matched 'Job'!\t");
 }
 
 static void interval_handler(Job *current_job, char *token)
 {
-    puts("Matched 'interval'!");
+    printf("Matched 'interval'!\t");
 }
 
 static void command_handler(Job *current_job, char *token)
 {
-    puts("Matched 'command'!");
+    printf("Matched 'command'!\t");
+}
+
+static void reaction_handler(Job *current_job, char *token)
+{
+    printf("Matched 'reaction'!\t");
 }
 
 static void init_tokens(void) 
 {
     nr_of_tokens = 0;
 
-    tokens[0] = create_token("Job",
-            "Job\\s+\".*\"",
+    // ^Job\\s+\"(.*)\"\\s+(.*?)(?:\\r*\\n){2}
+
+    tokens[0] = create_token("job",
+            "^Job\\s+\"([^\"]*)\"\\s+((\n?.)*)",
             job_handler);
-            
+
     tokens[1] = create_token("command",
-            "run\\s+`.*`",
-            command_handler);  
-            
+            "run\\s+`(.*)`",
+            command_handler);
+
     tokens[2] = create_token("interval",
-            "every\\s+[0-9]+\\s+(s|sec|second|seconds)|(m|min|minute|minutes)|(h|hour|hours)",
+            "every\\s*([0-9]+)?\\s+(hours|hour|h|minutes|minute|min|m|seconds|second|sec|s)",
             interval_handler);
+
+    tokens[3] = create_token("reaction",
+            "when\\s+`(.*)`\\s*((outputs)\\s+\"(.*)\")?",
+            reaction_handler);
 }
 
 static Token create_token(const char *name, const char *pattern, void (*handler)(Job *, char *))
@@ -76,32 +87,48 @@ static Token create_token(const char *name, const char *pattern, void (*handler)
 
 int main(void) 
 {
-    const char lines[] = {"Job \"Test\"\nrun `echo \"hello\"`\nevery 3 minutes\nJob \"Test 2\"\nrun `echo \"hello 2\"\nevery second`"};
+    char lines[] = {
+"Job \"Test\" \n\
+    run `this command`\n\
+    every 5 seconds\n\
+\n\
+Job \"Test 2\" run `that command` every second\n\
+\n\
+Job \"Test\" run `echo \"hello\"` every 3 minutes\n\
+\n\
+Job \"Test 2\"\n\
+run `echo \"hello 2\"`\n\
+every second\n\n\
+\
+Job \"Test 3\"\n\
+run `echo \"every 3 minutes\"`\n\
+when `bspc subscribe desktop`\n\n\
+\
+Job \"Test 4\"\n\
+run `whoami`\n\
+when `usb_connected.sh` outputs \"true\""};
+
     init_tokens();
 
-    char *tok = NULL;
-
-    tok = strtok(lines, "\n");
-    
+    int jobs_found = 0;
     Job jobs[10];
     Job *current_job = &(jobs[0]);
 
-    while ((tok = strtok(NULL, "\n")) != NULL)
+    printf("%s\n", lines);
+    int MAX_GROUPS = 5;
+
+    for (int i = 0; i < nr_of_tokens; i++)
     {
-        for (int i = 0; i < nr_of_tokens; i++)
+        regmatch_t matches[MAX_GROUPS]; 
+        long offset = 0;
+
+        while (regexec(tokens[i].pattern, lines + offset, MAX_GROUPS, matches, 0) == 0)
         {
-            int return_val = regexec(tokens[i].pattern, lines[i], 0, NULL, 0);
-            
-            if (return_val == 0) 
-            {
-                tokens[i].handler(current_job, tok);
-            }
-            else if (return_val != REG_NOMATCH)
-            {
-                puts("Error");
-            }
+            tokens[i].handler(current_job, lines);
+            printf("%d - %d\n", offset + matches[2].rm_so, offset + matches[2].rm_eo);
+            offset += matches[0].rm_eo;
         }
     }
-    
+
     return 0;
 }

6588183e86d4f991f23c18eebcbd33d3093cea32
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2018-11-01 23:54:13 +0100

    Cleans up repo

--- scripts/update_badges.sh
+++ scripts/update_badges.sh
@@ -1,42 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cov_badge_name="coverage"
 cov_badge_color="lightgrey"
 test_badge_name="tests"
 test_badge_color="brightgreen"
-coverage=$(make -s cov | grep TOTAL | awk {'print $4'} | cut -d '%' -f 1)
+coverage=$(make -s cov | grep TOTAL | awk "{'print $4'}" | cut -d '%' -f 1)
 tests_passed=0
 tests_failed=0
 
-while read line
+while read -r line
 do
     if [[ ! -z $line ]]
     then
-        passed=$(echo "$line" | grep -o "[0-9]\+ succeeded" | cut -d ' ' -f 1)
-        failed=$(echo "$line" | grep -o "[0-9]\+ failed" | cut -d ' ' -f 1)
-        let tests_passed+=${passed}
-        let tests_failed+=${failed}
+        passed=$(echo "$line" | grep -o "[0-9]\\+ succeeded" | cut -d ' ' -f 1)
+        failed=$(echo "$line" | grep -o "[0-9]\\+ failed" | cut -d ' ' -f 1)
+        (( tests_passed+=passed ))
+        (( tests_failed+=failed ))
     fi
-done <<< $(make -s test)
+done <<< "$(make -s test)"
 
 if (( $tests_failed > 0 ))
 then
     test_badge_color="red"
 fi
 
-if (( $coverage < 50 ))
+if (( coverage < 50 ))
 then
     cov_badge_color="red"
-elif (( $coverage < 70 ))
+elif (( coverage < 70 ))
 then
     cov_badge_color="orange"
-elif (( $coverage < 80 ))
+elif (( coverage < 80 ))
 then
     cov_badge_color="yellow"
-elif (( $coverage < 90 ))
+elif (( coverage < 90 ))
 then
     cov_badge_color="yellowgreen"
-elif (( $coverage < 95 ))
+elif (( coverage < 95 ))
 then
     cov_badge_color="green"
 else
